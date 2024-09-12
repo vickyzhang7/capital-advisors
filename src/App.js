5ea4480a94a929a0a88ad7093d5cdbd80a7224fa
weiqi <weiqi997998@gmail.com>
2024-09-11 22:54:46 -0400

    Teampage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Homepage from './pages/Home/Landing'; // Capitalize the component name
+import Teampage from './pages/Team/Teampage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           {/* Use 'element' instead of 'component' */}
           <Route path="/" element={<Homepage />} />
+          <Route path="/our-team" element={<Teampage />} />
         </Routes>
       </div>
     </div>
